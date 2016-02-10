0ddbd9b4db7dbc4f0bb48a74c609736b2cc239e0
Giancarlo Ventura <sakya_stelios@hotmail.com>
2016-02-09 23:47:39 -0500

    First prototype

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,6 +1,35 @@
 angular.module('starter.controllers', [])
 
-.controller('DashCtrl', function($scope) {})
+.controller('DashCtrl', function($scope) {
+  //Generate a list of persons for a group
+
+  var chance = new Chance();
+  var genders = ["male", "female"];
+  $scope.persons = [];
+
+  var Person = function(name, age, gender){
+    this.name = chance.name({ gender: gender });
+    this.age = chance.age();
+    this.gender = gender;
+  }
+
+function generatePersons(){
+  for (var i = 7 - 1; i >= 0; i--) {
+    var r = getRandomIntInclusive(0, 1);
+    var p = new Person(undefined, undefined, genders[r]);
+    $scope.persons.push(p);
+  }
+  localStorage.persons = "";
+  localStorage.persons = JSON.stringify($scope.persons);
+}
+
+  function getRandomIntInclusive(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+
+  generatePersons();
+
+})
 
 .controller('ChatsCtrl', function($scope, Chats) {
   // With the new view caching in Ionic, Controllers are only called
@@ -11,10 +40,7 @@ angular.module('starter.controllers', [])
   //$scope.$on('$ionicView.enter', function(e) {
   //});
 
-  $scope.chats = Chats.all();
-  $scope.remove = function(chat) {
-    Chats.remove(chat);
-  };
+  $scope.persons = JSON.parse(localStorage.persons);
 })
 
 .controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
